4fcaa0012e40a49b1a439224d65fb504e922f64e
qiuyangy@usc.edu <qiuyangy@usc.edu>
2016-05-23 22:28:11 -0700

    sort object returned

--- index.js
+++ index.js
@@ -13,44 +13,118 @@ app.use(multer({ dest: '/tmp/'}).array('image'));
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+function filter(obj) {
+
+  var res = [];
+  for (var i = 0; i < obj.length; i++) {
+    if (obj.streamStartTime) {
+      res.push(obj[i]);
+    };  
+
+  };
+
+
+
+  return res;
+}
+
+function compare(a,b) {
+  if (a.numberofcalling < b.numberofcalling)
+    return 1;
+  else if (a.numberofcalling > b.numberofcalling)
+    return -1;
+  else 
+    return 0;
+}
+
+
+function process(jsonfile){
+  var obj = JSON.parse(jsonfile);
+
+  //element:[streamCreator, numberofcalling, sumofcallingtime]
+
+  var dict = [];
+  var re = [];
+
+  for (var i = 0; i < obj.length; i++) {
+    if (dict.indexOf(obj[i].streamCreator) == -1) {
+        dict.push(obj[i].streamCreator);
+        var sumofcallingtime = 0;
+        var numberofcalling = 0;
+
+        for (var j = i; j < obj.length; j++) {
+
+          if (obj[j].streamCreator == obj[i].streamCreator) {
+            numberofcalling++;
+            if (obj[j].streamLength==""||obj[j].streamLength>1081||obj[j].streamLength<1) {
+                continue;
+            };
+            sumofcallingtime+=obj[j].streamLength;
+          }
+        };
+
+        var element = {"streamCreator": obj[i].streamCreator, "numberofcalling":numberofcalling,
+        "sumofcallingtime":sumofcallingtime};
+        re.push(element);
+    };
+  };
+
+
+  re.sort(compare);
+
+
+  return re
+
+}
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 })
 
 
+
+
+
 app.get('/process_get', function (req, res) {
 
-   // 输出 JSON 格式
-   response = {
-       Filename:req.query.first_name
-   };
-   console.log(response);
+
+      //Converter Class 
+      var Converter = require("csvtojson").Converter;
+      var converter = new Converter({});
+       
+      //end_parsed will be emitted once parsing finished 
+      converter.on("end_parsed", function (jsonArray) {
+
+        var json = JSON.stringify(jsonArray)
+
+        // console.log(json); //here is your result jsonarray 
+        var resArray = process(json);
 
 
+        console.log(resArray);
+
+        res.end(JSON.stringify(resArray));
+
+      });
+       
+      //read from file 
+      fs.createReadStream(__dirname + "/tmp_file/"+req.query.Filename).pipe(converter);
 
 
-
-   
-   res.end(JSON.stringify(response));
 })
 
-
-app.post('/process_post', urlencodedParser, function (req, res) {
-
-   // 输出 JSON 格式
-   response = {
-       first_name:req.body.first_name,
-       last_name:req.body.last_name
-   };
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
 
 app.post('/file_upload', function (req, res) {
-
-  // console.log(req)
-
 
    console.log(req.files[0]);  // 上传的文件信息
 
@@ -59,10 +133,7 @@ app.post('/file_upload', function (req, res) {
         fs.writeFile(des_file, data, function (err) {
          if( err ){
               console.log( err );
-         }else{
-         
          }
-
           res.end( req.files[0].filename );
        });
    });
