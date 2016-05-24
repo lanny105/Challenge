77611cdafea8385096e33ca8ce6f5acc1f895be0
qiuyangy@usc.edu <qiuyangy@usc.edu>
2016-05-24 16:04:10 -0700

    v1.0

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var moment = require('moment');
 
 
 var bodyParser = require('body-parser');
 var multer  = require('multer');
 
 
-app.use(express.static('public'));   //图片，css都在public下
+app.use(express.static('public'));   
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ dest: '/tmp/'}).array('image'));
 
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
+
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-function filter(obj) {
+function filter(obj,days) {
 
-  var res = [];
+  if (days == -1) {
+    return obj;
+  };
+
+  var re = [];
   for (var i = 0; i < obj.length; i++) {
-    if (obj.streamStartTime) {
-      res.push(obj[i]);
+
+
+    if (obj[i].objectId == "") {
+      continue;
+    };
+
+    if (moment().subtract(days, 'days').valueOf()<moment(obj[i].streamStartTime).valueOf()) {
+      re.push(obj[i]);
     };  
 
   };
 
-
-
-  return res;
+  return re;
 }
 
 function compare(a,b) {
@@ -33,13 +48,22 @@ function compare(a,b) {
     return 1;
   else if (a.numberofcalling > b.numberofcalling)
     return -1;
-  else 
-    return 0;
+  else {
+    if (a.sumofcallingtime < b.sumofcallingtime)
+      return 1;
+    else if (a.sumofcallingtime > b.sumofcallingtime)
+      return -1;
+    else
+      return 0;
+  }
+    
 }
 
 
-function process(jsonfile){
+function process(jsonfile,days){
   var obj = JSON.parse(jsonfile);
+
+  obj = filter(obj,days);
 
   //element:[streamCreator, numberofcalling, sumofcallingtime]
 
@@ -55,11 +79,13 @@ function process(jsonfile){
         for (var j = i; j < obj.length; j++) {
 
           if (obj[j].streamCreator == obj[i].streamCreator) {
-            numberofcalling++;
+            
             if (obj[j].streamLength==""||obj[j].streamLength>1081||obj[j].streamLength<1) {
                 continue;
             };
-            sumofcallingtime+=obj[j].streamLength;
+            numberofcalling++;
+            sumofcallingtime+=Number(obj[j].streamLength);
+            
           }
         };
 
@@ -78,25 +104,43 @@ function process(jsonfile){
 }
 
 
-
-
-
-
-
-
-
-
-
 app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 })
 
 
+app.get('/result', function (req, res) {
 
+  // res.writeHead(200,{"Content-Type":"text/html"});
+
+  if (req.query.Filename==null) {
+        console.log('filename missing!');
+        res.render("result.html", {name:""});
+        return;
+  };
+
+   res.render("result.html", { name: req.query.Filename });
+})
 
 
 app.get('/process_get', function (req, res) {
 
+
+      if (req.query.Filename==null||req.query.Nums ==null||req.query.Filename=="") {
+        console.log('arguments missing!');
+        res.json(JSON.stringify([]));
+        return;
+      };
+
+      try{
+         fs.accessSync(__dirname + "/tmp_file/" + req.query.Filename, fs.R_OK | fs.W_OK)
+      }catch(e){
+         //error
+         console.log('files missing!');
+         res.json(JSON.stringify([]));
+         return;
+
+      }
 
       //Converter Class 
       var Converter = require("csvtojson").Converter;
@@ -105,22 +149,45 @@ app.get('/process_get', function (req, res) {
       //end_parsed will be emitted once parsing finished 
       converter.on("end_parsed", function (jsonArray) {
 
+
+        fin = [];
+
         var json = JSON.stringify(jsonArray)
 
         // console.log(json); //here is your result jsonarray 
-        var resArray = process(json);
 
 
-        console.log(resArray);
+        if (req.query.Nums == -1) {
 
-        res.end(JSON.stringify(resArray));
+          var resArray = process(json,1);
+
+          fin.push(resArray);
+
+          resArray = process(json,7);
+
+          fin.push(resArray);
+
+          resArray = process(json,30);
+
+          fin.push(resArray);
+
+          resArray = process(json,-1);
+
+          fin.push(resArray);
+        }
+
+        else {
+          resArray = process(json,req.query.Nums);
+
+          fin.push(resArray);
+        }
+
+        res.json(JSON.stringify(fin));
+
 
       });
-       
-      //read from file 
+
       fs.createReadStream(__dirname + "/tmp_file/"+req.query.Filename).pipe(converter);
-
-
 })
 
 
@@ -146,6 +213,6 @@ var server = app.listen(8081, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+  console.log("start application http://%s:%s", host, port)
 
 })
